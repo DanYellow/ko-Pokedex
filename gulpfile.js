d7b96a2d293c65240de879af958e1940b5e1615f
Dan42K <jeanlouis.danielo@sfr.fr>
2016-01-28 20:23:06 +0100

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -17,20 +17,31 @@ var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var assign = require('lodash.assign');
 var babelify = require("babelify");
+var gutil = require('gulp-util');
 
 var customOpts = {
   entries: ['dev/assets/scripts/app.js'],
   debug: true
 };
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts).transform("babelify", {extensions: [".js"]})); 
 
-gulp.task('browserify', function() {
-    return browserify('./dev/assets/scripts/app.js')
-      .transform("babelify", {extensions: [".js"]})
-        .bundle()
-        //Pass desired output filename to vinyl-source-stream
-        .pipe(source('app.js'))
-        // Start piping stream to tasks!
-        .pipe(gulp.dest('./public/'));
-});
+// add transformations here
+// i.e. b.transform(coffeeify);
+
+gulp.task('browserify', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
+
+function bundle() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    
+    .pipe(source('app.js'))
+    // optional, remove if you don't need to buffer file contents
+    // Add transformation tasks to the pipeline here.
+    .pipe(gulp.dest('./public'));
+}
 
 gulp.task('default', ['templates', 'browserify']);
